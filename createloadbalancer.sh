dcddc23a33480b6a3980c74d3f0c6c5b802c1652
ssivaraj@hawk.iit.edu <ssivaraj@hawk.iit.edu>
2016-10-11 23:16:32 +0000

    entered launch config details using command prompt

--- createloadbalancer.sh
+++ createloadbalancer.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #creating load balancer name given by user
-echo -n "enter the loadbalancer name:"
+echo -n "Enter the loadbalancer name:"
 read input
 aws elb create-load-balancer --load-balancer-name $input --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets subnet-d1d0aba7
 # Enter the AMI ID, KEY-NAME
-echo -n "enter the AMI ID:"
+echo -n "Enter the AMI ID: "
 read input_ami
-echo -n "enter KEY-NAME:"
+echo -n "Enter KEY-NAME: "
 read input_keyname
-echo -n "enter sercurity groups"
+echo -n "Enter sercurity groups: "
 read input_security
-aws autoscaling create-launch-configuration --launch-configuration-name itmo544shyamconfig --image-id $input_ami --security-group $input_security --key-name $input_keyname --instance-type t2.micro --user-data file://hello.sh
-#echo "value is $input_loadbalancername"
+echo -n "What name you wat to give for launch configuration???"
+read input_launchconfig
+aws autoscaling create-launch-configuration --launch-configuration-name $input_launchconfig --image-id $input_ami --security-group $input_security --key-name $input_keyname --instance-type t2.micro --user-data file://hello.sh 
+echo "launching configuration..........."
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name server-rg --launch-configuration $input_launchconfig --availability-zone us-west-2b --load-balancer-name $input --max-size 5 --min-size 2 --desired-capacity 3
+echo "creating instances................"
