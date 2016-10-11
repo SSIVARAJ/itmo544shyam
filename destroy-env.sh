b34cf5446e9004e90eb482d6d67bc68f462cc090
ssivaraj@hawk.iit.edu <ssivaraj@hawk.iit.edu>
2016-10-11 19:26:48 +0000

    added the detach code load balancer

--- destroy-env.sh
+++ destroy-env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 Instance=`aws ec2 describe-instances --filters "Name=instance-state-code,Values=16" --query 'Reservations[*].Instances[*].InstanceId'`
-aws autoscaling set-desired-capacity --auto-scaling-group-name server-rg --desired-capacity 0
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name server-sd --min-size 0 --max-size 0 --desired-capacity 0
 
 aws ec2 wait instance-terminated --instance-ids $Instance
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name server-rg
+aws autoscaling detach-load-balancers --load-balancer-names itmo544shyamload --auto-scaling-group-name server-sd
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name server-sd
 aws autoscaling delete-launch-configuration --launch-configuration-name itmo544shyamconfig
-aws elb delete-load-balancer --load-balancer-name itmo544shyam
+aws elb delete-load-balancer --load-balancer-name itmo544shyamload
