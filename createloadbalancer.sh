0fb06c08af9a93a906c58db57a1d164890674123
ssivaraj@hawk.iit.edu <ssivaraj@hawk.iit.edu>
2016-10-12 04:33:11 +0000

    Updated the query

--- createloadbalancer.sh
+++ createloadbalancer.sh
@@ -38,11 +38,25 @@ fi
 
 echo -n "Enter sercurity groups: "
 read input_security
+if [ -z "$input_security" ]
+        then
+                while [ -z "$input_security" ]
+                do
+                        echo -n "Please enter the security group: "
+                        read input_security
+                done
+fi
 
-
-echo -n "What name you wat to give for launch configuration???"
+echo -n "What name you want to give for launch configuration???"
 read input_launchconfig
-
+if [ -z "$input_launchconfig" ]
+        then
+                while [ -z "$input_launchconfig" ]
+                do
+                        echo -n "Please enter the Launch configuration: "
+                        read input_launchconfig
+                done
+fi
 aws autoscaling create-launch-configuration --launch-configuration-name $input_launchconfig --image-id $input_ami --security-group $input_security --key-name $input_keyname --instance-type t2.micro --user-data file://hello.sh 
 echo "launching configuration..........."
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name server-rg --launch-configuration $input_launchconfig --availability-zone us-west-2b --load-balancer-name $input --max-size 5 --min-size 2 --desired-capacity 3
